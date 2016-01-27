4c64695193a4cc1ef8d39b76a411194ca674f6b3
zhb <zhb@zhb-PC>
2016-01-27 22:12:26 +0800

    m

---
+++ scuthnwebpages/WebContent/js/public/publicScript.js
@@ -0,0 +1,48 @@
+function ccr($this){
+     $this.css("color","#FF9900");
+
+}
+function ccg($this){
+     $this.css("color","#CCFF00");
+
+}
+function ccb($this){
+     $this.css("color","#00CCFF");
+}
+function ccB($this){
+     $this.css("color","#000000");
+}
+function ccG($this){
+    $this.css("color","#666666");
+}
+function cc($this){
+    $this.css("color","#FFFFFF");
+}
+
+function cc_1($this){
+    $this.css("color","#000000");
+}
+
+function cc_transparent($this){
+    $this.css("color","transparent");
+}
+
+function cbcr($this){
+    $this.css("background-color","#FF9900");
+}
+
+function cbcg($this){
+    $this.css("background-color","#CCFF00");
+}
+
+function cbcb($this){
+    $this.css("background-color","#00CCFF");
+}
+
+function cbc($this){
+    $this.css("background-color","#FFFFFF");
+}
+
+function cbc_transparent($this){
+	$this.css("background-color","transparent");
+}
